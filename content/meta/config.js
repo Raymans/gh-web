f6de8464bfbcdee3898437ea30afd80b21307c10
raymans <raymans86@gmail.com>
2018-05-30 00:56:50 +0800

    update logo, icon, description

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,28 +1,26 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteTitle: "GeekHub", // <title>
+  shortSiteTitle: "GeekHub", // <title> ending for posts and pages
+  siteDescription: "GeekHub is a GatsbyJS starter.",
   siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Raymans",
+  authorTwitterAccount: "raymans",
   // info
-  headerTitle: "greg lobinski",
+  headerTitle: "Practice interview as a game",
   headerSubTitle: "presents another one GatsbyJS starter",
   // manifest.json
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "GeekHub",
+  manifestShortName: "GeekHub", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
   manifestDisplay: "standalone",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/raymans" }
   ]
 };
