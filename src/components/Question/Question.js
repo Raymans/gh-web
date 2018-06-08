dd2ec68189368bac0a1e8a67cd4fe047e2c5f0bb
Raymans Peng <rpeng@digitalriver.com>
2018-06-08 15:04:55 +0800

    temp question form

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,12 @@
 /* eslint no-unused-vars: 0 */
 
 import { navigateTo } from "gatsby-link";
-import Button from "antd/lib/button";
-import Form from "antd/lib/form";
-import Input from "antd/lib/input";
+import { Form, Input, Button, Tabs, Icon, Checkbox } from 'antd';
 import PropTypes from "prop-types";
 import React from "react";
 
 const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
 const { TextArea } = Input;
 import "antd/lib/form/style/index.css";
 import "antd/lib/input/style/index.css";
@@ -15,6 +14,7 @@ import "antd/lib/button/style/index.css";
 import { ThemeContext } from "../../layouts";
 
 let CodeMirror = null;
+let uuid = 0;
 if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
   CodeMirror = require('react-codemirror');
   require('codemirror/mode/javascript/javascript');
@@ -22,14 +22,59 @@ if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
 }
 
 const Question = props => {
-  const { getFieldDecorator } = props.form;
-
+  const { getFieldDecorator, getFieldValue } = props.form;
+  
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 4 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 20 },
+    },
+  };
+  const formItemLayoutWithOutLabel = {
+    wrapperCol: {
+      xs: { span: 24, offset: 0 },
+      sm: { span: 20, offset: 4 },
+    },
+  };
+  
+  const remove = (k) => {
+    const { form } = props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one answer
+    if (keys.length === 1) {
+      return;
+    }
+    
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  }
+  
+  const add = () => {
+    const { form } = props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(uuid);
+    uuid++;
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  }
+  
   function encode(data) {
     return Object.keys(data)
       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
   }
-
+  
   function handleSubmit(e) {
     e.preventDefault();
     props.form.validateFields((err, values) => {
@@ -39,7 +84,7 @@ const Question = props => {
       }
     });
   }
-
+  
   function sendMessage(values) {
     fetch("/", {
       method: "POST",
@@ -52,14 +97,52 @@ const Question = props => {
       })
       .catch(error => {
         console.error("Form submission error:", error);
-        this.handleNetworkError();
+        handleNetworkError();
       });
   }
-
+  
   function handleNetworkError(e) {
     console.log("submit Error");
   }
-
+  
+  getFieldDecorator('keys', { initialValue: [] });
+  const keys = getFieldValue('keys');
+  const formItems = keys.map((k, index) => {
+    return (
+      <FormItem
+        {...(formItemLayout)}
+        label={index + 1}
+        required={false}
+        key={k}
+      >
+      
+      {getFieldDecorator(`names[${k}]`, {
+        validateTrigger: ['onChange', 'onBlur'],
+        rules: [{
+          required: true,
+          whitespace: true,
+          message: "Please input answer and check if corrected",
+          initialValue: { number: 0, currency: 'rmb' },
+        }],
+      })(
+        <span>
+            <Checkbox />
+            <Input placeholder="Please input answer and check if corrected" style={{ width: '60%', marginRight: 8 }} />
+          </span>
+      )}
+        {keys.length > 1 ? (
+          <Icon
+            className="dynamic-delete-button"
+            type="minus-circle-o"
+            disabled={keys.length === 1}
+            onClick={() => remove(k)}
+          />
+        ) : null}
+    </FormItem>
+    
+    );
+  });
+  
   return (
     <React.Fragment>
       <div className="form">
@@ -83,28 +166,39 @@ const Question = props => {
                       required: true,
                       message: "Please enter description!",
                       whitespace: true,
-
+                      
                     }
                   ]
                 })(<Input placeholder="please input question's description"/>)}
               </FormItem>
-              {CodeMirror &&
-                <FormItem label="code">
-                  {getFieldDecorator("code", {
-                    rules: [
-                      {
-                        required: true,
-                        message: "Please enter description!",
-                        initialValue: 'function(){}'
-                      }
-                    ],
-                    initialValue: 'function(){}'
-                  })(
-
-                    <CodeMirror options={{ lineNumbers: true, mode: 'javascript' }}/>
-                  )}
-                </FormItem>
-              }
+              <Tabs defaultActiveKey="2">
+                <TabPane tab={<span><Icon type="code-o" />Coding</span>} key="1">
+                  {CodeMirror &&
+                  <FormItem label="code">
+                    {getFieldDecorator("code", {
+                      rules: [
+                        {
+                          required: true,
+                          message: "Please enter description!",
+                          initialValue: 'function(){}'
+                        }
+                      ],
+                      initialValue: 'function(){}'
+                    })(
+                      <CodeMirror options={{ lineNumbers: true, mode: 'javascript' }}/>
+                    )}
+                  </FormItem>
+                  }
+                </TabPane>
+                <TabPane tab={<span><Icon type="check-square" />Multiple Question</span>} key="2">
+                  {formItems}
+                  <FormItem {...formItemLayoutWithOutLabel}>
+                    <Button type="dashed" onClick={add} style={{ width: '60%' }}>
+                      <Icon type="plus" /> Add Answer
+                    </Button>
+                  </FormItem>
+                </TabPane>
+              </Tabs>
               <FormItem>
                 <Button type="primary" htmlType="submit">
                   Submit
@@ -152,6 +246,21 @@ const Question = props => {
                   .form :global(input) {
                     max-width: 50%;
                   }
+                }
+                :global(.dynamic-delete-button) {
+                  cursor: pointer;
+                  position: relative;
+                  top: 4px;
+                  font-size: 24px;
+                  color: #999;
+                  transition: all .3s;
+                }
+                :global(.dynamic-delete-button:hover) {
+                  color: #777;
+                }
+                :global(.dynamic-delete-button[disabled]) {
+                  cursor: not-allowed;
+                  opacity: 0.5;
                 }
               `}</style>
             </Form>
