62331258622e609fdc3eb76a6bdf9483771ac71d
Raymans Peng <rpeng@digitalriver.com>
2018-07-13 11:29:06 +0800

    Fix build error (Webpack build error), due to localStorage (window) is not defined and async/await not supported, hide prettier lint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,19 @@
-import config from '../../content/meta/config'
+import config from '../../content/meta/config';
 import request from '../utils/request';
+
+let _localStorage;
+if (typeof window !== 'undefined' && window) {
+  _localStorage = localStorage;
+}else{
+  let localStorageMemory = require('localstorage-memory');
+  _localStorage = localStorageMemory;
+}
 
 export async function signup(params) {
   return request(`${config.ghServiceUrl}/api/users`,{
     method: 'POST',
     data: params
-  });
+  }).then(user => _localStorage.setItem("userid", user.id));
 }
 export async function getUser(params) {
   return request(`${config.ghServiceUrl}/api/users`,{
@@ -16,7 +24,7 @@ export async function getUser(params) {
 
 export function getUserUrl() {
   return {
-    authUrl: `${config.ghServiceUrl}/api/users/${localStorage.getItem('userid')}`,
+    authUrl: `${config.ghServiceUrl}/api/users/${_localStorage.getItem('userid')}`,
     reqOptions: {
       'method': 'GET',
       'headers': {
