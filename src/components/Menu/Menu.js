e9973e821cdff49c345d1462a5f4699e76eb8966
raymans <raymans86@gmail.com>
2018-07-24 08:47:56 +0800

    template for user info avatar.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 require('core-js/fn/array/from');
 
 import Link from 'gatsby-link';
-import {Menu as AntMenu, Icon} from 'antd';
+import {Menu as AntMenu, Icon, Avatar} from 'antd';
 import {AuthConsumer} from 'react-check-auth';
 
 class Menu extends React.Component {
@@ -72,6 +72,13 @@ class Menu extends React.Component {
     );
 
   render() {
+    const userSetting = (
+      <AntMenu.SubMenu title={<span><Avatar src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-1/p32x32/28782617_10155159912751319_8014460284062164976_n.jpg?_nc_cat=0&oh=f9ef27fcf0cdc8cd3d215c141afa75b2&oe=5BB64F0A"/>Raymans</span>}>
+        <AntMenu.Item>
+          <Icon type='logout'/>Login out
+        </AntMenu.Item>
+      </AntMenu.SubMenu>
+    );
     return (
       <AuthConsumer>
         {({userInfo}) => {
@@ -86,7 +93,7 @@ class Menu extends React.Component {
                 this.items.map(item => {
                   if (item.label === 'Login') {
                     if (userInfo) {
-                      return <span>Hi, {userInfo.username}</span>
+                      return userSetting;
                     }
                     return this.renderItem(item);
                   }
